1512dfc9d8961924a126ef3bdb45c9bdceaaa2d9
peptuban22 <pep.tuban@gmail.com>
2022-02-15 15:50:34 +0700

    update data token

--- token github.R
+++ token github.R
@@ -2,3 +2,22 @@
 #token github
 
 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
+
+#token github
+
+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
+
+#git push using token
+#git push https://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@github.com/peptuban22/pep.git
+
+
+#git config --global user.email pep.tuban@gmail.com
+#git config --global user.name "Your Name"
+#git config --global user.name "peptuban22"
+#git config --global user.email pep.tuban@gmail.com
+
+
+install.packages("gitcreds")
+library(gitcreds)
+gitcreds_set()
+
